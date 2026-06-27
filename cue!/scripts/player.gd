de3adafdbaa983ba534b66c33bd465cb6964fc36
matt-ctrl-alt-del <vmatthewdaniel@gmail.com>
2026-06-26 17:40:18 -0700

    Added map, player, and else

---
+++ cue!/scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const SPEED = 110.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	# Handle jump.
+	if input_direction.x > 0:
+		animated_sprite.flip_h = false
+	elif input_direction.x < 0:
+		animated_sprite.flip_h = true
+
+	if input_direction != Vector2.ZERO:
+		velocity = input_direction * SPEED
+		animated_sprite.play("idle")
+	else:
+		velocity = Vector2.ZERO
+		animated_sprite.play("idle")
+		
+
+	move_and_slide()
